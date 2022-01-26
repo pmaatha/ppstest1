f5fa36efad3ecd6a382961917b761b2c2e86fee6
Mamatha P <97168002+pmaatha@users.noreply.github.com>
2022-01-26 09:40:43 +0000

    p2debug completed

---
+++ p1final.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int add(int x,int y)
+{
+  int k=y+x;
+  return k;
+}
+int main()
+{
+  int a,b;
+  printf("enter the two numbers to add\n");
+  scanf("%d%d",&a,&b);
+  printf("%d+%d=%d\n",a,b,add(a,b));
+  return 0;
+}
