aaff4c98c8bd413a0a92ba149a8fe91858c457b5
Mamatha P <97168002+pmaatha@users.noreply.github.com>
2022-01-30 16:04:23 +0000

    p6debug complited

---
+++ p7original.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int k;
+  printf ("enter the size of array:\n");
+  scanf("%d",&k);
+  return k;
+  }
+void array_input (int n, int *a)
+}
+printf ("enter the values\n")
