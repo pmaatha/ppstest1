9eeca854e34e31df1183a4aac33e73813072b61a
Mamatha P <97168002+pmaatha@users.noreply.github.com>
2022-01-30 16:35:31 +0000

    p6final complited

--- p7original.c
+++ p7original.c
@@ -7,5 +7,36 @@ int input_array_size()
   return k;
   }
 void array_input (int n, int *a)
+{
+printf ("enter the values\n");
+for (int i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
 }
-printf ("enter the values\n")
+}
+int sum_n_arrays(int n,int a[n])
+{
+  int sum=0;
+  for(int i=0;i,n;i++)
+  {
+    sum=sum+a[i];
+  }
+  return sum;
+}
+void output(int n, int a[n], int sum)
+{
+  for(int i=0;i<n-1;i++)
+  {
+    printf("%d+",a[i]);
+  }
+  printf("%d=%d\n",a[n-1],sum);
+}
+int main()
+{
+  int n,sum;
+  n=input_array_size();
+  int a[n];
+  array_input(n,a);
+  sum=sum_n_arrays(n,a);
+  output(n,a,sum);
+}
